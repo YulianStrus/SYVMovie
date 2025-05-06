9608ef94d815b466f2e5b31229e65a04036a9b4c
Yulian <50151425+YulianStrus@users.noreply.github.com>
2025-05-07 02:12:44 +0300

    Update MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,16 +5,20 @@ function MovieList({ movies, searchTerm, toggleFavorite }) {
     return (
         <div className="movie-list">
             {movies
-                .filter(movie => movie.title.toLowerCase().includes(searchTerm.toLowerCase()))
-                .map(movie => (
+                .filter(
+                    (movie) =>
+                        movie.title &&
+                        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+                .map((movie) => (
                     <div key={movie.id} className="movie-card">
-                        <img src={movie.image} alt={movie.title} width="100"/>
+                        <img src={movie.image} alt={movie.title} width="100" />
                         <button className="favorite" onClick={() => toggleFavorite(movie.id)}>
                             {movie.favorite ? "★" : "☆"}
                         </button>
                         <h3>{movie.title}</h3>
                         <p>Rating: {movie.rating}</p>
-                        <p>Release Date: <br/> {movie.releaseDate}</p>
+                        <p>Release Date: <br /> {movie.releaseDate}</p>
 
                         <Link to={`/movie/${movie.id}`}>View Details</Link>
                     </div>
@@ -23,7 +27,7 @@ function MovieList({ movies, searchTerm, toggleFavorite }) {
                 <div className="movie-card add-movie">+</div>
             </Link>
         </div>
-);
+    );
 }
 
 export default MovieList;
